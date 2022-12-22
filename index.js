db38330e2f5b5bc9fe9a5e2710a793f6aece80c4
nasibbekova <118672049+nasibbekova@users.noreply.github.com>
2022-12-22 13:22:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ for (let item of companies) {
 
         item.tax +=itemTwo
 
-        taxMax = companies.reduce((a,b) => {
+        let maxTax = companies.reduce((a,b) => {
             if (a.tax > b.tax){
                 return a
             } else {
@@ -119,7 +119,7 @@ for (let item of companies) {
             }
         })
 
-        taxMin= companies.reduce((a,b) => {
+        let minTax = companies.reduce((a,b) => {
             if (a.tax > b.tax){
                 return a
             } else {
@@ -127,9 +127,12 @@ for (let item of companies) {
             }
         })
 
-        averageExpenses = total / companies.length
+        minTax.push(taxMin)
+
+        maxTax.push(taxMax)
+        
+        total / companies.length.push(averageExpenses)
 }
-
 
 // Найти общий tax со всех компаний
 // Найти кто меньше всех платит налог и кто больше всех платит налог
